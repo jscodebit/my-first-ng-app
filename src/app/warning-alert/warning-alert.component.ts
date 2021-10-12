44d2f0f87964319a6a9c42636ae3a342f08ea372
jscodebit <soujanya.janapatla@gmail.com>
2021-10-12 11:44:11 -0400

    Learning how to create components

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styles: [`
+  p {
+    color: yellow;
+  }`]
+})
+export class WarningAlertComponent {
+
+}
